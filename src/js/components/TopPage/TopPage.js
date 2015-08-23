480242774d08f683e96e31a5c7141093918d9f94
timakin <timaki.st@gmail.com>
2015-08-23 19:04:47 +0900

    [add] package and toppage

---
+++ src/js/components/TopPage/TopPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class TopPage extends React.Component {
+    render() {
+        return(
+           <div>
+                <h1>Timakinのほ〜むぺ〜じ</h1>
+           </div>
+        );
+    }
+}
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var TopPage = require('./components/TopPage/TopPage');
+
+React.render(
+    <TopPage />
+    document.getElementById('content')
+);
