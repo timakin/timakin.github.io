e8e4d1fbcbf183e3c4d79132a1f850bc20534c04
timakin <timaki.st@gmail.com>
2015-08-28 00:34:23 +0900

    [fix] css

--- src/js/components/Home/Home.js
+++ src/js/components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Router from 'react-router';
+import CSS from '../../../css/home/home.css';
 var {Link} = Router;
 
 export default class Home extends React.Component {
     render() {
         return(
-           <div>
+           <div className={CSS.home}>
                 <Link to="github">github</Link>
                 <h1>Timakinのほ〜むぺ〜じ</h1>
            </div>
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel'
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
             }
         ]
     },
